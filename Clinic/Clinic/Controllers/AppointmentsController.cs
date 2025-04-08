b22ba730832bc146ed5b9061e3dbd88e6e2d80d4
GalkaJakub <kubabb2002@gmail.com>
2025-04-08 22:10:51 +0200

    Appointments Controller added, Index and Create new Appointment

---
+++ Clinic/Clinic/Controllers/AppointmentsController.cs
@@ -0,0 +1,64 @@
+﻿using Clinic.Data;
+using Clinic.Enums;
+using Clinic.Models;
+using Clinic.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public AppointmentsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var appointments = db.Appointments.Include(x => x.Doctor).Include(x => x.Patient).ToList();
+            return View(appointments);
+        }
+
+        public IActionResult Create()
+        {
+            var model = new AppointmentVM
+            {
+                Appointment = new Appointment(),
+                Patients = db.Patients.ToList(),
+                Doctors = db.Doctors.ToList(),
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AppointmentVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Appointment.Status = AppointmentStatus.Awaiting;
+                model.Appointment.RegistrationDate = DateTime.Now;
+                // TODO: Current Receptionist Id in the future
+                model.Appointment.ReceptionistId = db.Receptionists.First().UserId;
+                db.Appointments.Add(model.Appointment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            model.Patients = db.Patients.ToList();
+            model.Doctors = db.Doctors.ToList();
+            return View(model);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
--- Clinic/Clinic/Models/Appointment.cs
+++ Clinic/Clinic/Models/Appointment.cs
@@ -7,13 +7,17 @@ namespace Clinic.Models
     {
         [Key]
         public int AppointemntId { get; set; }
+        [Required]
         public string Description { get; set; }
         public string? Diagnosis { get; set; }
         public AppointmentStatus Status { get; set; }
         public DateTime RegistrationDate { get; set; }
+        [Required(ErrorMessage = "Appointment Date is required")]
         public DateTime? AppointmentDate { get; set; }
+        [Required(ErrorMessage = "Doctor is required")]
         public int DoctorId { get; set; }
         public Doctor? Doctor { get; set; }
+        [Required(ErrorMessage = "Patient is required")]
         public int PatientId { get; set; }
         public Patient? Patient { get; set; }
         public int ReceptionistId { get; set; }
