0898264bb9b395b646d400f752f15e7b5f3eae9e
JakubG708 <agnel113322@gmail.com>
2025-04-09 15:46:24 +0200

    added patient view, controller, changes to patient model

---
+++ Clinic/Clinic/Controllers/PatientsController.cs
@@ -0,0 +1,53 @@
+﻿using Clinic.Data;
+using Clinic.Enums;
+using Clinic.Models;
+using Clinic.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Controllers
+{
+    public class PatientsController : Controller
+    {
+
+        private readonly ApplicationDbContext db;
+
+        public PatientsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var patients = db.Patients.Include(x => x.Adress).ToList();
+            return View(patients);
+        }
+
+        public IActionResult Create()
+        {
+            var model = new PatientVM
+            {
+                Patient = new Patient(),
+                Addresses = db.Addresses.ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(PatientVM model)
+        {
+
+            // TODO: Find out why patient is never valid
+            if (ModelState.IsValid)
+            {
+
+                db.Patients.Add(model.Patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            model.Addresses = db.Addresses.ToList();
+            return View(model);
+        }
+    }
+}
--- Clinic/Clinic/Models/Patient.cs
+++ Clinic/Clinic/Models/Patient.cs
@@ -6,8 +6,18 @@ namespace Clinic.Models
     {
         [Key]
         public int PatientId { get; set; }
+        [Required(ErrorMessage = "PESEL is required.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "PESEL must be exactly 11 digits.")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "PESEL must contain only digits.")]
         public string PESEL { get; set; }
+
+
+        [Required(ErrorMessage = "Name is required.")]
+        [RegularExpression(@"^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$", ErrorMessage = "Name must start with a capital letter and contain only letters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname is required.")]
+        [RegularExpression(@"^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$", ErrorMessage = "Surname must start with a capital letter and contain only letters.")]
         public string Surname { get; set; }
         public int AdressId { get; set; }
         public Address Adress { get; set; }
