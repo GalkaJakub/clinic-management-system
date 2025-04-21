88c5122a63c98adfad05d18a12d94f62adcf4db0
JakubG708 <agnel113322@gmail.com>
2025-04-21 14:46:42 +0200

    LabExam Post works properly

--- Clinic/Clinic/Areas/LabTechnician/Controllers/LabExamsController.cs
+++ Clinic/Clinic/Areas/LabTechnician/Controllers/LabExamsController.cs
@@ -5,6 +5,7 @@ using Clinic.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 
 
@@ -42,6 +43,60 @@ namespace Clinic.Areas.LabTechnician.Controllers
                 ExamSelection = labExam?.ExamSelection
             };
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult LabExam(LabExamVM model)
+        {
+
+            ModelState.Remove("LabExam.Appointment");
+            ModelState.Remove("LabExam.LabTechnician");
+            ModelState.Remove("LabExam.HeadLabTechnician");
+            ModelState.Remove("LabExam.ExamSelection");
+
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .Select(x => new { x.Key, x.Value.Errors });
+                foreach (var error in errors)
+                {
+                    Console.WriteLine($"Key: {error.Key}, Errors: {string.Join(", ", error.Errors)}");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                var newLabExam = model.LabExam;
+                var labExam = db.LabExams.Find(newLabExam.LabExamId);
+                if (labExam != null)
+                {
+
+                    labExam.DoctorsNotes = newLabExam.DoctorsNotes;
+                    labExam.RequestDate = newLabExam.RequestDate;
+                    labExam.Result = newLabExam.Result;
+                    labExam.ExamDate = newLabExam.ExamDate;
+                    labExam.HeadLabNotes = newLabExam.HeadLabNotes;
+                    labExam.AcceptDate = newLabExam.AcceptDate;
+                    labExam.Status = ExamStatus.InProgress;
+
+                    labExam.AppointmentId = newLabExam.AppointmentId;
+                    labExam.LabTechnicianId = newLabExam.LabTechnicianId;
+                    labExam.HeadLabTechnicianId = newLabExam.HeadLabTechnicianId;
+                    labExam.ExamSelectionId = newLabExam.ExamSelectionId;
+
+                    db.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("Lab exam is null");
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            
+
+
+                return View(model);
         }
 
     }
